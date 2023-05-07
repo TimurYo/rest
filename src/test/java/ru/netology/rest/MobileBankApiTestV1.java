14cef0414308aca8e5b305c78828f037e3f3b87b
SabirovTimur <sabirovtimurcfc@gmail.com>
2023-05-07 18:02:31 +0300

    Initial commit

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -0,0 +1,30 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+      // Given - When - Then
+      // Предусловия
+      given()
+          .baseUri("http://localhost:9999/api/v1")
+      // Выполняемые действия
+      .when()
+          .get("/demo/accounts")
+      // Проверки
+      .then()
+          .statusCode(200)
+              .contentType(ContentType.JSON)
+              .body("",hasSize(3))
+              .body("[1].currency",equalTo("USD"))
+              .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+
+    }
+}
